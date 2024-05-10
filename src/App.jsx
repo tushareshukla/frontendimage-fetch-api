7934218bf3045d6efc85ed45f336a632869af4de
bhudev <tushareshukla@gmail.com>
2024-05-10 22:56:51 +0530

    completed-toggle

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,34 @@ function App() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://my-json-server.typicode.com/Codeinwp/front-end-internship-api/posts')
-      .then(response => response.json())
-      .then(data => setPosts(data))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch(
+      "https://my-json-server.typicode.com/Codeinwp/front-end-internship-api/posts"
+    )
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
-  return <div className="card-container" style={{ margin: "120px 190px"}}>
-  {
-    posts.map((post) => (<ImageComponent key={post.id}  imageUrl={post.thumbnail.small} title={post.title}  author={post.author.name} role={post.author.role} description={post.content} 
-      imageDate={new Date(post.date * 1000).toLocaleDateString('en-US', {
-      month: 'short',
-      day: '2-digit',
-      year: 'numeric'
-    })} />))
-  }
-  
-  </div>;
+  return (
+    <div className="card-container" style={{ margin: "120px 190px" }}>
+      {posts.map((post) => (
+        <ImageComponent
+        profile={post.author.avatar}
+          key={post.id}
+          imageUrlBig={post.thumbnail.large}
+          imageUrl={post.thumbnail.small}
+          title={post.title}
+          author={post.author.name}
+          role={post.author.role}
+          description={post.content}
+          imageDate={new Date(post.date * 1000).toLocaleDateString("en-US", {
+            month: "short",
+            day: "2-digit",
+            year: "numeric",
+          })}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/assets/Component/ImageComponent.jsx
+++ src/assets/Component/ImageComponent.jsx
@@ -1,33 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ImageComponent.css";
 const ImageComponent = ({
   imageUrl,
   title,
   author,
   description,
-  imageAlt,
   imageDate,
   role,
+  imageUrlBig,
+  profile
 }) => {
+  const [expanded, setExpanded] = useState(false);
+  const toggleExpansion = () => {
+    console.log("clicked");
+    setExpanded(!expanded);
+  };
   return (
-    <div className="card">
-      <img src={imageUrl} alt={imageAlt} />
-      <div class="overlay" onClick={{}}>Learn More</div>
-      <div className="dots">
-        <span className="dot-blue"></span>
-        <span className="dot-yellow"></span>
-      </div>
-      <div className="card-content">
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <div className="card-footer">
-          <span>
-            {author} - {role}
-          </span>
-          <span>{imageDate}</span>
+    <>
+      <div className="card">
+        <img src={imageUrl} alt={imageUrl} />
+        <div className="overlay" onClick={toggleExpansion}>
+          Learn More
+        </div>
+        <div className="dots">
+          <span className="dot-blue"></span>
+          <span className="dot-yellow"></span>
+        </div>
+        <div className="card-content">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <div className="card-footer">
+            <span>
+              {author} - {role}
+            </span>
+            <span>{imageDate}</span>
+          </div>
         </div>
       </div>
+      {expanded && (
+  <div className="big-card">
+  <span className="close" onClick={toggleExpansion}>
+    x
+  </span>
+  <img src={imageUrlBig} alt={imageUrlBig} />
+  <div className="big-card-content">
+    <h3>{title}</h3>
+    <p>{description}</p>
+    <div className="big-card-footer">
+        <img src={profile} alt={profile}/>
+      <div className="big-card-footer-text">
+        {author} - {role}
+      </div>
+    
     </div>
+  </div>
+</div>
+      )}
+    </>
   );
 };
 

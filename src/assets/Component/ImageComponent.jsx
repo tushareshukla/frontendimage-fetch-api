41d913514202fd74787ecc3dd0a1a65a516dc6d6
bhudev <tushareshukla@gmail.com>
2024-05-10 21:17:07 +0530

    last-on-cards

--- src/assets/Component/ImageComponent.jsx
+++ src/assets/Component/ImageComponent.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "./ImageComponent.css";
-const ImageComponent = ({imageUrl,title,author,description,imageAlt,imageDate, role}) => {
+const ImageComponent = ({
+  imageUrl,
+  title,
+  author,
+  description,
+  imageAlt,
+  imageDate,
+  role,
+}) => {
   return (
     <div className="card">
       <img src={imageUrl} alt={imageAlt} />
+      <div className="dots">
+        <span class="dot-blue"></span>
+        <span class="dot-yellow"></span>
+      </div>
       <div className="card-content">
         <h3>{title}</h3>
-        <p>
-         {description}
-        </p>
+        <p>{description}</p>
         <div className="card-footer">
-          <span>{author} - {role}</span>
+          <span>
+            {author} - {role}
+          </span>
           <span>{imageDate}</span>
         </div>
       </div>
